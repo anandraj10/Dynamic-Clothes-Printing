85714a95f78da7c254149eae03458d15e82eff3a
anandraj10 <anandraj36401@gmail.com>
2025-04-28 18:25:06 +0530

    First commit

---
+++ frontend/client/vite.config.js
@@ -0,0 +1,8 @@
+// This is a Vite configuration file for a frontend project using Tailwind CSS.
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
